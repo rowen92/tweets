14ff566bdd8a0529eb24ebe77378a1bf7894596a
rowen92 <maksym.pakhn@gmail.com>
2016-07-19 18:41:09 +0300

    fixed

--- app/index.js
+++ app/index.js
@@ -1,5 +1,51 @@
 "use strict";
 
-var Tweet = require("./tweet");
-var tweet = new Tweet();
-console.log( tweet.getTweets("neiva") );
+/**
+ * Represents tweets
+ * @module app/index
+ */
+
+var tokens = require("./tokens"),
+  OAuth = require( "oauth" ),
+  oauth = new OAuth.OAuth(
+    "https://api.twitter.com/oauth/request_token",
+    "https://api.twitter.com/oauth/access_token",
+    tokens.access.consumerKey,
+    tokens.access.consumerSecret,
+    "1.0A",
+    null,
+    "HMAC-SHA1"
+  ),
+  nickName = "goneiva464",
+  i = 0;
+
+/**
+ * Get the all tweets using Twitter API
+ * @param  {Object}   https: tokens.access.tokenKey,
+ *                          tokens.access.tokenSecret,
+ *                          function( error, data, res Parse tweets
+ * @return {JSON}     List of tweets
+ */
+oauth.get(
+  "https://api.twitter.com/1.1/statuses/user_timeline.json?screen_name=" + nickName,
+  tokens.access.tokenKey,
+  tokens.access.tokenSecret,
+  function( error, data, res ) {
+    var lengthOfDataJSON,
+        tweets = [],
+        dataJSON;
+
+    if ( error ) {
+      throw new Error( error );
+    }
+
+    dataJSON = JSON.parse( data );
+    lengthOfDataJSON = dataJSON.length;
+
+    for ( ; i < lengthOfDataJSON; i++ ) {
+      tweets.push( dataJSON[ i ].text );
+    }
+
+    console.log( tweets );
+  }
+);
